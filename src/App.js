bcb5cc1007f848764fa5bc617a911e5784051a06
monnibhadu123 <106861189+monnibhadu123@users.noreply.github.com>
2022-11-19 20:51:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import NavBar from './components/NavBar';
-import Sreach from './components/Sreach'
+import Sreach from './components/Search'
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
        <NavBar />
-       <Sreach />
+       <Search />
     </div>
  
   );
