3385945a61fc14050c78e3062dbf4b816cc49c7b
monnibhadu123 <106861189+monnibhadu123@users.noreply.github.com>
2022-11-19 15:37:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import NavBar from './components/NavBar';
+import Sreach from './components/Sreach'
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
-   
+       <NavBar />
+       <Sreach />
     </div>
+ 
   );
 }
