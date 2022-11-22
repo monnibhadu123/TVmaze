a62bbde99aaaf116cb710d839ecca50cc725ecca
monnibhadu123 <106861189+monnibhadu123@users.noreply.github.com>
2022-11-22 21:39:47 +0530

    Update Show.js

--- src/components/Show.js
+++ src/components/Show.js
@@ -1,110 +1,100 @@
+import axios from "axios";
 import React, { useState, useEffect } from "react";
+import NotFound from "./NotFound";
 
-function Actor() {
-    const [inputVal, setInputVal] = useState("");
-    const [showData, setshowData] = useState([]);
+function Show() {
+  const [input, setInput] = useState("");
+  const [showData, setShowData] = useState([]);
 
-    let dataUrl = "";
-    if (inputVal.length > 0) {
-        dataUrl = `https://api.tvmaze.com/search/shows?q=${inputVal}`;
-    } 
+  const handleShowInput = (e) => {
+    setInput(e.target.value);
+  };
 
-    const getshowData = async () => {
-        try {
-            let respone = await fetch(dataUrl);
-            let resData = await respone.json();
-            setshowData(resData);
-        } catch (error) {
-            console.log(error);
-        }
-    };
-    useEffect(() => {
-        getshowData();
-    },[inputVal]);
-    // console.log(showData);
-    return (
-        <>
-            <section className="mt-5 show-wrapper">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-7">
-                            <input
-                                type="text"
-                                value={inputVal}
-                                onChange={(e) => setInputVal(e.target.value)}
-                                className="form-control input-show"
-                                placeholder="Search by Show name...."
-                            />
-                        </div>
+  const getshowData = () => {
+    const url = `https://api.tvmaze.com/search/shows?q=${input}`;
+    axios.get(url).then((res) => setShowData(res.data));
+  };
+
+  useEffect(() => {
+    getshowData();
+  }, [input]);
+
+  return (
+    <>
+      <section className="mt-2 show-wrapper">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <input
+                type="text"
+                className="form-control input-show mt-3 mb-3"
+                value={input}
+                onChange={handleShowInput}
+                placeholder="Search by show name..."
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="container mt-4">
+        <div className="row">
+          {showData && showData.length > 0
+            ? showData &&
+              showData.map((item) => {
+                const regex = /(<([^>]+)>)/gi;
+                let image = item.show.image;
+
+                return image ? (
+                  <div className="col-md-4 mb-3" key={item.show.id}>
+                    <div className="card">
+                      <h5 className="text-danger text-center mt-2 mb-2">
+                        {item.show.name}
+                      </h5>
+                      <a href={item.show.url} target="_moni">
+                        {item.show.image ? (
+                          <img
+                            src={item.show.image.medium}
+                            style={{ width: "100%" }}
+                            alt={
+                              item.show.name != null
+                                ? item.show.name
+                                : "Not found"
+                            }
+                          />
+                        ) : (
+                          ""
+                        )}
+                      </a>
+                      <div className="card-body">
+                        <p
+                          className="card-text overflow-hidden"
+                          style={{ height: "90px" }}
+                        >
+                          {item.show.summary !== null
+                            ? item.show.summary.replace(regex, "")
+                            : ""}
+                        </p>
+                        <span>
+                          <i
+                            className="fa fa-star text-success me-2"
+                            aria-hidden="true"
+                          ></i>
+                          {item.show.rating.average}
+                        </span>
+                      </div>
                     </div>
-                </div>
-            </section>
-
-            <section>
-                <div className="container mt-4">
-                    <div className="row">
-                        {showData.map((element) => {
-                            return (
-                              <div className="col-md-3 mb-3">
-                                <div className="card">
-                                  <a href={element.show.url} target="_moni">
-                                  {element.show.image ? (
-                                    <img
-                                      src={element.show.image.medium}
-                                        style={{
-                                        width: "255px",
-                                        height: "325px",
-                                        }}
-                                      alt={
-                                        element.show.name != null
-                                          ? element.show.name
-                                          : "Not found"
-                                      }
-                                    />
-                                  ) : (
-                                    <div
-                                      className="poster"
-                                      style={{ height: "325px" }}
-                                    >
-                                      <img
-                                        src="https://www.prokerala.com/movies/assets/img/no-poster-available.jpg"
-                                        style={{
-                                        width: "270px",
-                                        height: "325px",
-                                        }}
-                                        alt = {element.show.name}
-                                      />
-                                    </div>
-                                  )}
-                                  </a>
-                                  <div className="card-body">
-                                    {/* <p
-                                      className="card-text overflow-hidden"
-                                      style={{ height: "90px" }}
-                                    >
-                                      {element.show.summary}
-                                    </p> */}
-                                    <span>
-                                      <i
-                                        class="fa fa-star text-success"
-                                        aria-hidden="true"
-                                      ></i>{" "}
-                                      {element.show.rating.average}
-                                    </span>
-                                  </div>
-
-                                  <h5 className="text-danger text-center">
-                                    {element.show.name}
-                                  </h5>
-                                </div>
-                              </div>
-                            );
-                        })}
-                    </div>
-                </div>
-            </section>
-        </>
-    );
+                  </div>
+                ) : (
+                  ""
+                );
+              })
+            : <NotFound />}
+        </div>
+      </section>
+     
+    </>
+  );
 }
 
-export default Actor;
+export default Show;
