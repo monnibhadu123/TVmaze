3b19e1f3bb9ad3fafbb552088907fe2a4d182acf
monnibhadu123 <106861189+monnibhadu123@users.noreply.github.com>
2022-11-22 21:41:15 +0530

    Create NotFound.jsx

---
+++ src/components/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function NotFound() {
+  return (
+    <>
+        <p className='fw-bold mb-3 text-danger fs-5'>Not found any data...</p>
+    </>
+  )
+}
+
+export default NotFound;
